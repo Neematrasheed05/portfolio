73c68ceddd0a9103015c8c3e09ae552caa070276
Neematrasheed05 <nymatrasheed05@gmail.com>
2023-12-26 11:31:35 +0300

    created routes successfully

--- abunajma-app/src/App.js
+++ abunajma-app/src/App.js
@@ -1,10 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Nabar/navbar";
+import Footer from "./Components/Footer/footer";
+
+
+import './App.css';
+import About from "./Components/About/about";
+import Contact from "./Components/Contact/contact";
+import Home from "./Components/Home/home";
+import Skills from "./Components/Skills/skills";
+import Services from "./Components/Services/services";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>React + Redux + Thunk</h1>
-      </header>
+      <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+
+      <Footer />
+    </Router>
      </div>
 
   );
---
+++ abunajma-app/src/Components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(){
+    return(
+    <div>
+     <footer>
+      <p>&copy; 2023 Your Company</p>
+     </footer>
+     </div>
+    )
+}
+export default Footer;
