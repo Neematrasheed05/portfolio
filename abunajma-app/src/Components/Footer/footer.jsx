19dcdc1c6f269050d11f0a33ef5e729ccebc9975
Neematrasheed05 <nymatrasheed05@gmail.com>
2024-01-02 23:06:34 +0300

    done with homepage

--- abunajma-app/src/Components/Footer/footer.jsx
+++ abunajma-app/src/Components/Footer/footer.jsx
@@ -1,35 +1,45 @@
 import React from "react";
-import {Link } from 'react-router-dom'
-import {FaMailBulk ,FaPhone, FaTiktok } from 'react-icons/fa'
+import { Link } from 'react-router-dom';
+import { FaMailBulk, FaPhone, FaTiktok, FaYoutube } from 'react-icons/fa';
+import "./footer.css";
 
-function Footer(){
-    return(
+function Footer() {
+  return (
     <div>
       <div className='footer'>
-            <div className='footer-container'>
+        <div className='footer-container'>
+
+          <div className='phone'>
+            <h4>
+              <FaPhone size={20} className="mr-2" />+254718199654
+            </h4>
+          </div>
+
+          <div className='email'>
+            <Link to='pajmaanajuu@gmail.com' className="text-white">
+              <h4 className="d-flex align-items-left">
+                <FaMailBulk size={30} className="mr-2" />
                 
-                    <div className='phone'>
-                        <h4><FaPhone size={20} style={{ color:"#fff", marginRight:"2rem" }} />+254718199654 or  +254717595965</h4>
-                    </div>
+              </h4>
+            </Link>
+          </div>
 
-                    <div className='email'>
-                       <Link to='pajmaanajuu@gmail.com'> <h4><FaMailBulk size={20} style={{ color:"#fff", marginRight:"2rem" }} />pajmaanajuu@gmail.com</h4></Link>
-                    </div>
-
-                </div>
-
-                    <div className='social'>
-                        
-                        <Link to='https://www.tiktok.com/@abunajmawithkids?_t=8ih3c09GuTG&_r=1'> <FaTiktok  size={30} style={{ color:'#fff', marginRight:'1rem'}}/></Link>
-                    </div>
+          <Link to='https://www.tiktok.com/@abunajmawithkids?_t=8ih3c09GuTG&_r=1' className="text-white d-flex align-items-center">
+            <FaTiktok size={30} className="mr-2" />
             
-            </div>
+          </Link>
 
-        
-     
-      <p>&copy; 2023 Your Company</p>
-     
-     </div>
-    )
+          <Link to='https://www.youtube.com/watch?v=PZVeLjOenIA&list=PLd6S1FUPB462VoKeMmtB6L-Muspk1f_04' className="text-white d-flex align-items-center">
+            <FaYoutube size={30} className="mr-2" />
+
+          </Link>
+
+        </div>
+
+        <p className="mt-3">&copy; 2024 AbuNajmaWithKids</p>
+      </div>
+    </div>
+  );
 }
+
 export default Footer;
